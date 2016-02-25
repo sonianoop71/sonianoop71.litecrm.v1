76b3558708a7dc7413068c5c2ca282ab27ee729c
Anoop Soni <soni.anoop71@gmail.com>
2016-02-25 15:53:20 +0530

    Pushed video of 30 mb.
    Signed-off-by: Anoop Soni <soni.anoop71@gmail.com>

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -14,10 +14,7 @@ var urlsToCache = [
   '',
   'css/main.css',
   'index.html',
-  'images/1.jpg',
-  'images/2.jpg',
-  'images/3.jpg',
-  'images/4.jpg'
+  'videos/1.webm'
 ];
 
 self.addEventListener('install', function(event) {
